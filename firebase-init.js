39c57cd0d3d1c74796ead1926c8a37fe3784c0f4
ureteneller44 <ureteneller44@gmail.com>
2025-11-22 01:45:20 +0300

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,17 @@
+// firebase-init.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTiuZ3bvY4PJpXKNS2megAcwjo-omYOYM",
+  authDomain: "hayatiisler-abde5.firebaseapp.com",
+  projectId: "hayatiisler-abde5",
+  storageBucket: "hayatiisler-abde5.firebasestorage.app",
+  messagingSenderId: "761325880458",
+  appId: "1:761325880458:web:084633651cc08429c6d973",
+  measurementId: "G-6HLHC9794J"
+};
+
+// Firebase başlat
+firebase.initializeApp(firebaseConfig);
+
+// Firestore referansı
+const db = firebase.firestore();
