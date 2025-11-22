ab8f6b94796ea61d8dfce66f0fae13f4fd4d8896
ureteneller44 <ureteneller44@gmail.com>
2025-11-22 18:31:32 +0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -4,17 +4,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyBTiuZ3bvY4PJpXKNS2megAcwjo-omYOYM",
   authDomain: "hayatiisler-abde5.firebaseapp.com",
   projectId: "hayatiisler-abde5",
-  storageBucket: "hayatiisler-abde5.appspot.com", // dursun, sorun değil
+  storageBucket: "hayatiisler-abde5.appspot.com",
   messagingSenderId: "761325880458",
   appId: "1:761325880458:web:084633651cc08429c6d973",
   measurementId: "G-6HLHC9794J"
 };
 
-// Firebase başlat
 firebase.initializeApp(firebaseConfig);
 
 // SADECE Firestore
 const db = firebase.firestore();
-
-// !!! BURADA firebase.storage() YOK !!!
-// const storage = firebase.storage();  <-- BUNU KESİNLİKLE SİL / YORUM SATIRI YAP
